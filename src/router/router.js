78662cf8a92e0a744a9a8f5cd48d75419e385be9
kenneth898 <kennethtan0601@gmail.com>
2025-05-26 12:29:29 +0800

    add 404page

--- src/router/router.js
+++ src/router/router.js
@@ -1,5 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomePage from '../views/homepage.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomePage from '../views/homepage.vue';
+import NotFound from '@/views/404.vue';
 
 const routes = [
 	{
@@ -7,6 +8,15 @@ const routes = [
 		name: 'Home',
 		component: HomePage
 	},
+	{
+		path: '/nullwinbox-register',
+		redirect: '/'
+	},
+	{
+		path: '/:pathMatch(.*)*',
+		name: 'NotFound',
+		component: NotFound
+	}
 	// Add more routes here
 ]
 
