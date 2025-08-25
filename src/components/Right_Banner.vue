c5b23318ea0c920a5e7750eea7497e1e7a52b026
kenneth898 <kennethtan0601@gmail.com>
2025-08-25 12:28:35 +0800

    year auto

--- src/components/Right_Banner.vue
+++ src/components/Right_Banner.vue
@@ -55,7 +55,7 @@
 					</div>
 					<div class="time-info">
 						<p>{{ $t('message.start_time') }}</p>
-						<p>17 Aug 2024</p>
+						<p>{{ $t('message.date_info', { year: new Date().getFullYear() }) }}</p>
 						<p class="time">03:00</p>
 						<a :href="link_atascasinobet + 'atas-login'" rel="nofollow">
 							<div class="bet-now-container">
