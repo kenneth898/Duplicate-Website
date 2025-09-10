1ed7232925d98d5cc3bc7e0c54335711048854b3
kenneth898 <kennethtan0601@gmail.com>
2025-09-10 16:58:39 +0800

    update year auto

--- src/components/Right_Banner.vue
+++ src/components/Right_Banner.vue
@@ -131,7 +131,7 @@
 					</div>
 					<div class="time-info">
 						<p>{{ $t('message.start_time') }}</p>
-						<p>28 Sep 2024</p>
+						<p>{{ $t('message.date', { year: new Date().getFullYear() }) }}</p>
 						<p class="time">19:30</p>
 						<a href="/">
 							<div class="bet-now-container">
